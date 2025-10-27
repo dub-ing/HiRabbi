a09a1cd5eca04c986d7c7659df0d6947ba45c3e5
dub-ing <duboyeolawale@gmail.com>
2025-10-27 02:18:22 -0700

    added local data for sermons

---
+++ src/data/sermons.js
@@ -0,0 +1,96 @@
+export const sermons = [
+  {
+    topic: "What To Say",
+    date: "Aug 27 2025",
+    pastor: "Pastor Mark Adewusi",
+    memoryVerse:
+      "Ye are of God, little children and have overcome them: because greater is he that is in you, than he that is in the world. -",
+    bibleReads: ["1 John 4:4", "Mark 11:23", "Proverbs 6;2"],
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum amet corrupti unde repudiandae voluptatem natus quasi eos nam ut quas omnis sapiente tempora eaque et velit, culpa inventore autem error aliquid qui soluta fuga magnam consequatur tempore. Voluptatem, architecto quo, fugit velit quasi molestias nisi nemo hic iure beatae porro!",
+    image: "bibleOpen.jpg",
+    questions: [
+      {
+        question: "How do i know what to say when i am guilt filled ?",
+        date: "Aug 27 2025",
+        id: 1,
+        name: "John Wick",
+        answer:
+          "ipsum dolor sit amet consectetur adipisicing elit. Eum amet corrupti unde repudiandae voluptatem natus quasi eos nam ut quas omnis sapiente tempora eaque et velit, culpa inventore autem error aliquid qui",
+        upvotes: 5,
+        status: "answered",
+      },
+      {
+        question: "How do i know what to say when i am guilt filled ?",
+        date: "Aug 27 2025",
+        id: 1,
+        name: "John Wick",
+        answer: "",
+        upvotes: 1,
+        status: "pending",
+      },
+    ],
+  },
+  {
+    topic: "What I Say",
+    date: "Sep 3 2025",
+    pastor: "Pastor Simi Loveth",
+    memoryVerse:
+      "Ye are of God, little children and have overcome them: because greater is he that is in you, than he that is in the world. -",
+    bibleReads: ["1 John 4:4", "Mark 11:23", "Proverbs 6;2"],
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum amet corrupti unde repudiandae voluptatem natus quasi eos nam ut quas omnis sapiente tempora eaque et velit, culpa inventore autem error aliquid qui soluta fuga magnam consequatur tempore. Voluptatem, architecto quo, fugit velit quasi molestias nisi nemo hic iure beatae porro!",
+    image: "bibleOpen.jpg",
+    questions: [
+      {
+        question: "How do i know what to say when i am guilt filled ?",
+        date: "Sep 3 2025",
+        id: 1,
+        name: "Delilah Esther",
+        answer:
+          "ipsum dolor sit amet consectetur adipisicing elit. Eum amet corrupti unde repudiandae voluptatem natus quasi eos nam ut quas omnis sapiente tempora eaque et velit, culpa inventore autem error aliquid qui",
+        upvotes: 25,
+        status: "answered",
+      },
+      {
+        question: "How do i know what to say when i am guilt filled ?",
+        date: "Sep 4 2025",
+        id: 2,
+        name: "Nathan Vick",
+        answer: "",
+        upvotes: 1,
+        status: "pending",
+      },
+      {
+        question: "How do i know what to say when i am guilt filled ?",
+        date: "Sep 3 2025",
+        id: 3,
+        name: "Delilah Esther",
+        answer:
+          "ipsum dolor sit amet consectetur adipisicing elit. Eum amet corrupti unde repudiandae voluptatem natus quasi eos nam ut quas omnis sapiente tempora eaque et velit, culpa inventore autem error aliquid qui",
+        upvotes: 15,
+        status: "answered",
+      },
+      {
+        question: "How do i know what to say when i am guilt filled ?",
+        date: "Sep 3 2025",
+        id: 4,
+        name: "John Wick",
+        answer: "",
+        upvotes: 10,
+        status: "pending",
+      },
+    ],
+  },
+  {
+    topic: "What Not Say",
+    date: "Sep 10 2025",
+    pastor: "Pastor Mark Adewusi",
+    memoryVerse:
+      "Ye are of God, little children and have overcome them: because greater is he that is in you, than he that is in the world. -",
+    bibleReads: ["1 John 4:4", "Mark 11:23", "Proverbs 6;2"],
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum amet corrupti unde repudiandae voluptatem natus quasi eos nam ut quas omnis sapiente tempora eaque et velit, culpa inventore autem error aliquid qui soluta fuga magnam consequatur tempore. Voluptatem, architecto quo, fugit velit quasi molestias nisi nemo hic iure beatae porro!",
+    image: "bibleOpen.jpg",
+  },
+];
